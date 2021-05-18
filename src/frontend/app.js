5ffa3351f51a328f9f97f456afaffece160ac629
Emmvish <76655197+Emmvish@users.noreply.github.com>
2021-05-18 17:55:49 +0530

    Update app.js

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -4,10 +4,17 @@ import { Provider } from 'react-redux';
 import 'normalize.css/normalize.css'
 import './styles/ReactStyles.css'
 
+import { login } from './actions/auth';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 
 const store = configureStore();
+
+const strAuth = localStorage.getItem('auth');
+if(strAuth) {
+    const auth = JSON.parse(strAuth);
+    store.dispatch(login({name: auth.name, jwt: auth.token}));
+}
 
 const jsx = (
     <Provider store={store}>
